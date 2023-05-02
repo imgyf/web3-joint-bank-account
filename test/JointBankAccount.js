3c665b5b51a47e4adeb6bdae231000052a234106
Yi Fan Goh <gohyf91@gmail.com>
2023-05-02 23:57:20 +0800

    fix: test cases

--- test/JointBankAccount.js
+++ test/JointBankAccount.js
@@ -20,6 +20,50 @@ describe("JointBankAccount", function () {
     })
   })
 
+  async function deployJointBankAccount() {
+    const [addr0, addr1, addr2, addr3] = await ethers.getSigners();
+
+    const BankAccount = await ethers.getContractFactory("JointBankAccount");
+    const bankAccount = await BankAccount.deploy();
+
+    return { bankAccount, addr0, addr1, addr2, addr3 };
+  }
+
+  async function deployJointBankAccountWithAccounts(
+    owners = 1,
+    deposit = 0,
+    withdrawlAmounts = []
+  ) {
+    const { bankAccount, addr0, addr1, addr2, addr3, addr4 } =
+      await loadFixture(deployJointBankAccount);
+    let addresses = [];
+
+    if (owners == 2) addresses = [addr1.address];
+    else if (owners == 3) addresses = [addr1.address, addr2.address];
+    else if (owners == 4)
+      addresses = [addr1.address, addr2.address, addr3.address];
+
+    await bankAccount.connect(addr0).createAccount(addresses);
+
+    if (deposit > 0) {
+      await bankAccount
+        .connect(addr0)
+        .deposit(0, { value: deposit.toString() });
+    }
+
+    for (const withdrawlAmount of withdrawlAmounts) {
+      await bankAccount.connect(addr0).requestWithdraw(0, withdrawlAmount);
+    }
+
+    return { bankAccount, addr0, addr1, addr2, addr3, addr4 };
+  }
+
+  describe("Deployment", () => {
+    it("should deploy without error", async () => {
+      await loadFixture(deployJointBankAccount);
+    })
+  })
+
   describe("Creating an account", () => {
     it("should allow creating a single user account", async () => {
       const { bankAccount, addr0 } = await loadFixture(deployJointBankAccount);
@@ -79,12 +123,139 @@ describe("JointBankAccount", function () {
   })
 
   describe("Depositing", () => {
-    it("should allow depositing into a single user account", async () => {
-      const { bankAccount, addr0 } = await loadFixture(deployJointBankAccount);
-      await bankAccount.connect(addr0).createAccount([]);
-      await bankAccount.connect(addr0).deposit(anyValue(), anyValue(), { value: 1 });
-      const balance = await bankAccount.connect(addr0).getBalance();
-      expect(balance).to.equal(1);
+    it("should allow deposit from account owner", async () => {
+      const { bankAccount, addr0 } = await deployJointBankAccountWithAccounts(1);
+      await expect(
+        bankAccount.connect(addr0).deposit(0, { value: "100" })
+      ).to.changeEtherBalances([bankAccount, addr0], ["100", "-100"]);
+    });
+
+    it("should NOT allow deposit from non-account owner", async () => {
+      const { bankAccount, addr1 } = await deployJointBankAccountWithAccounts(1);
+      await expect(bankAccount.connect(addr1).deposit(0, { value: "100" })).to
+        .be.reverted;
+    });
+  });
+
+  describe("Withdraw", () => {
+    describe("Request a withdraw", () => {
+      it("account owner can request withdraw", async () => {
+        const { bankAccount, addr0 } = await deployJointBankAccountWithAccounts(
+          1,
+          100
+        );
+        await bankAccount.connect(addr0).requestWithdraw(0, 100);
+      });
+
+      it("account owner can not request withdraw with invalid amount", async () => {
+        const { bankAccount, addr0 } = await deployJointBankAccountWithAccounts(
+          1,
+          100
+        );
+        await expect(bankAccount.connect(addr0).requestWithdraw(0, 101)).to.be
+          .reverted;
+      });
+
+      it("non-account owner cannot request withdraw", async () => {
+        const { bankAccount, addr1 } = await deployJointBankAccountWithAccounts(
+          1,
+          100
+        );
+        await expect(bankAccount.connect(addr1).requestWithdraw(0, 90)).to.be
+          .reverted;
+      });
+
+      it("non-account owner cannot request withdraw", async () => {
+        const { bankAccount, addr0 } = await deployJointBankAccountWithAccounts(
+          1,
+          100
+        );
+        await bankAccount.connect(addr0).requestWithdraw(0, 90);
+        await bankAccount.connect(addr0).requestWithdraw(0, 10);
+      });
+    });
+
+    describe("Approve a withdraw", () => {
+      it("should allow account owner to approve withdraw", async () => {
+        const { bankAccount, addr1 } = await deployJointBankAccountWithAccounts(
+          2,
+          100,
+          [100]
+        );
+        await bankAccount.connect(addr1).approveWithdraw(0, 0);
+        expect(await bankAccount.getApprovals(0, 0)).to.equal(1);
+      });
+
+      it("should not allow non-account owner to approve withdraw", async () => {
+        const { bankAccount, addr2 } = await deployJointBankAccountWithAccounts(
+          2,
+          100,
+          [100]
+        );
+        await expect(bankAccount.connect(addr2).approveWithdraw(0, 0)).to.be
+          .reverted;
+      });
+
+      it("should not allow owner to approve withdrawl multiple times", async () => {
+        const { bankAccount, addr1 } = await deployJointBankAccountWithAccounts(
+          2,
+          100,
+          [100]
+        );
+        bankAccount.connect(addr1).approveWithdraw(0, 0);
+        await expect(bankAccount.connect(addr1).approveWithdraw(0, 0)).to.be
+          .reverted;
+      });
+
+      it("should not allow creator of request to approve request", async () => {
+        const { bankAccount, addr0 } = await deployJointBankAccountWithAccounts(
+          2,
+          100,
+          [100]
+        );
+        await expect(bankAccount.connect(addr0).approveWithdraw(0, 0)).to.be
+          .reverted;
+      });
+    });
+
+    describe("Make withdraw", () => {
+      it("should allow creator of request to withdraw approved request", async () => {
+        const { bankAccount, addr0, addr1 } =
+          await deployJointBankAccountWithAccounts(2, 100, [100]);
+        await bankAccount.connect(addr1).approveWithdraw(0, 0);
+        await expect(
+          bankAccount.connect(addr0).withdraw(0, 0)
+        ).to.changeEtherBalances([bankAccount, addr0], ["-100", "100"]);
+      });
+
+      it("should not allow creator of request to withdraw approved request twice", async () => {
+        const { bankAccount, addr0, addr1 } =
+          await deployJointBankAccountWithAccounts(2, 200, [100]);
+        await bankAccount.connect(addr1).approveWithdraw(0, 0);
+        await expect(
+          bankAccount.connect(addr0).withdraw(0, 0)
+        ).to.changeEtherBalances([bankAccount, addr0], ["-100", "100"]);
+        await expect(bankAccount.connect(addr0).withdraw(0, 0)).to.be.reverted;
+      });
+
+      it("should not allow non-creator of request to withdraw approved request ", async () => {
+        const { bankAccount, addr1 } = await deployJointBankAccountWithAccounts(
+          2,
+          200,
+          [100]
+        );
+        await bankAccount.connect(addr1).approveWithdraw(0, 0);
+        await expect(bankAccount.connect(addr1).withdraw(0, 0)).to.be.reverted;
+      });
+
+      it("should not allow non-creator of request to withdraw approved request ", async () => {
+        const { bankAccount, addr0 } = await deployJointBankAccountWithAccounts(
+          2,
+          200,
+          [100]
+        );
+        await expect(bankAccount.connect(addr0).withdraw(0, 0)).to.be.reverted;
+      });
     });
   });
 });
